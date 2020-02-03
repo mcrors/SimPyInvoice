d7a84e1370da75b0d7e45f61802802d1d5097247
rory.houlihan <rory.houlihan@vistaprint.com>
2020-02-03 16:43:51 +0100

    setup for unit tests only

--- tests/unit_tests/test_simpyinvoice.py
+++ tests/unit_tests/test_simpyinvoice.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import pytest
 from simpyinvoice import create_app
 
 
 class TestSimPyInvoiceShould:
 
+    @pytest.mark.unit_test
     @staticmethod
     def test_create_flask_app():
         app = create_app()
