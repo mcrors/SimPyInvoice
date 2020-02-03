bbd02e0357fbc743ca436e5ef7f37de571bb108d
rory.houlihan <rory.houlihan@vistaprint.com>
2020-02-03 16:45:59 +0100

    remove staticmethod decorator

--- tests/unit_tests/test_simpyinvoice.py
+++ tests/unit_tests/test_simpyinvoice.py
@@ -6,7 +6,6 @@ from simpyinvoice import create_app
 class TestSimPyInvoiceShould:
 
     @pytest.mark.unit_test
-    @staticmethod
-    def test_create_flask_app():
+    def test_create_flask_app(self):
         app = create_app()
         assert isinstance(app, Flask)
